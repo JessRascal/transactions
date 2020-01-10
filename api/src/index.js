5d65c7e5f1d21d93bc6e60f3d45b32d25bdbca45
Jess Rascal <me@jessrascal.com>
2020-01-10 00:28:14 +0000

    Very basic foundation complete

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,21 @@
+import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
+import routes from './routes';
+
+import model, { connectDb } from './models';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(`This is a route`);
-})
+// app-wide middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-  console.log('API listening on port 3000');
+// routes
+app.use('/ping', routes.ping);
+
+// connect to db then start app
+connectDb().then(async () => {
+  app.listen(process.env.PORT, () => console.log(`API listening on port ${process.env.PORT}`));
 });
