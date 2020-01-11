3ad5128321f3fa02df53fa3f96135476446d574d
Jess Rascal <me@jessrascal.com>
2020-01-11 18:13:49 +0000

    WIP user routes

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -1,9 +1,21 @@
 import User from '../models/user.model';
 
 // create new user
+exports.create = (req, res) => {
+
+};
 
 // retrieve a single user
+exports.findOne = (req, res) => {
+
+};
 
 // update a note
+exports.update = (req, res) => {
+
+};
 
 // delete a note
+exports.delete = (req, res) => {
+
+};
--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -1,23 +1,52 @@
 import { Router } from 'express';
+import users from '../controllers/user.controller';
 
 const router = Router();
 
-// GET /users/
-router.get('/', (req, res) => {
-  res.json({ message: `Getting all users` });
-});
+// user endpoints
+// POST /users/
+router.post('/', users.create);
 
 // GET /users/:id
-router.get('/:id', (req, res) => {
-  // get user details
-  // console.log(`Endpoint test. ID: ${req.params.id}`);
-  res.json({ message: `Endpoint test. ID: ${req.params.id}` });
-});
+router.get('/:id', users.findOne);
 
-// BANK ACCOUNT ENDPOINTS
-// GET /users/:id/bankaccounts/
-router.get('/:id/bankaccounts/', (req, res) => {
-  // return all bank accounts for a user
-});
+// PUT /users/:id
+router.put('/:id', users.update); // TODO: change to PATCH
+
+// DELETE /users/:id
+router.delete('/:id', users.delete);
+
+// bank account endpoints
+// POST /users/bankaccounts/
+router.post('/bankaccounts', bankAccounts.create);
+
+// GET /users/bankaccounts/:id
+router.get('/bankaccounts/:id', bankAccounts.findOne);
+
+// PUT /users/bankaccounts/:id
+router.put('/bankaccounts/:id', bankAccounts.update); // TODO: change to PATCH
+
+// DELETE /users/bankaccounts/:id
+router.delete('/bankaccounts/:id', bankAccounts.delete);
+
+
+
+// // GET /users/
+// router.get('/', (req, res) => {
+//   res.json({ message: `Getting all users` });
+// });
+
+// // GET /users/:id
+// router.get('/:id', (req, res) => {
+//   // get user details
+//   // console.log(`Endpoint test. ID: ${req.params.id}`);
+//   res.json({ message: `Endpoint test. ID: ${req.params.id}` });
+// });
+
+// // BANK ACCOUNT ENDPOINTS
+// // GET /users/:id/bankaccounts/
+// router.get('/:id/bankaccounts/', (req, res) => {
+//   // return all bank accounts for a user
+// });
 
 export default router;
