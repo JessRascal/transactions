8fd6ae6b753049521a81a61bb82b5d97ae27b86d
Jess Rascal <me@jessrascal.com>
2020-01-18 23:28:45 +0000

    WIP: refactoring bank account UPDATE endpoint

--- api/src/middleware/bankAccount.middleware.js
+++ api/src/middleware/bankAccount.middleware.js
@@ -8,8 +8,23 @@ export async function findAccounts(req, res, next) {
       return res.status(404).json({ message: `User does not exist so unable to get accounts` });
     }
   } catch (err) {
-    return res.status(500).json({ message: `Cannot find accounts`, error: err });
+    return res.status(500).json(err);
   }
   req.bankAccounts = user.bankAccounts;
+  next();
+}
+
+export async function findSingleAccount(req, res, next) {
+  let user;
+  try {
+    account = await User.findOne({ _id: req.params.userId, 'bankAccounts._id': req.params.accountId });
+    console.log(`Account: ${account}`); // TODO: REMOVE
+    if (account == null) {
+      return res.status(404).json({ message: `Account does not exist` });
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+  req.bankAccount = account;
   next();
 }
--- api/src/routes/bankaccount.route.js
+++ api/src/routes/bankaccount.route.js
@@ -1,23 +1,23 @@
 import { Router } from 'express';
 import bankAccounts from '../controllers/bankAccount.controller';
-import { findAccounts } from '../middleware/bankAccount.middleware';
+import { findAccounts, findSingleAccount } from '../middleware/bankAccount.middleware';
 
 const router = Router({ mergeParams: true });
 
 // POST /users/:userId/bankaccounts/
 router.post('/', bankAccounts.create);
 
+// PATCH /users/:userId/bankaccounts/:accountId/
+router.patch('/:accountId', findSingleAccount, bankAccounts.update);
+
+// GET /users/:userId/bankaccounts/:accountId/
+router.get('/:accountId', bankAccounts.findOne);
+
 // MIDDLEWARE: get user details
 router.use('/', findAccounts);
 
 // GET /users/:userId/bankaccounts/
 router.get('/', bankAccounts.findAll);
-
-// GET /users/:userId/bankaccounts/:accountId/
-router.get('/:accountId', bankAccounts.findOne);
-
-// PATCH /users/:userId/bankaccounts/:accountId/
-router.patch('/:accountId', bankAccounts.update);
 
 // DELETE /users/:userId/bankaccounts/:accountId/
 router.delete('/:accountId', bankAccounts.delete);
